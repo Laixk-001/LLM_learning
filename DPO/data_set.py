1d573540e4762f7c90b75a68ff6ef9b1546ac5c8
赖笑慷 <laixiaokang@4paradigm.com>
2024-10-20 20:23:56 +0800

    update

--- DPO/data_set.py
+++ DPO/data_set.py
@@ -15,7 +15,7 @@ def load_data():
         train_data: 偏好对齐训练样本
         dev_data: 偏好对齐验证样本
     """
-    dataset_name="./data/zhihu_3k_rlhf"
+    dataset_name="./data/zhihu_3k_rlhf.tsv"
     ratio=0.1
     data_zh = load_dataset(path=dataset_name)
     data_all = data_zh['train'].train_test_split(ratio)
