da346e6e6c12f1aef871f71eb4911d2a1cd4b8e8
赖笑慷 <laixiaokang@4paradigm.com>
2024-10-20 20:18:19 +0800

    up

--- DPO/data_set.py
+++ DPO/data_set.py
@@ -4,7 +4,7 @@ from datasets import load_dataset
     数据集加载
 """
 
-def load_dataset(dataset_name="./data/zhihu_3k_rlhf",ratio=0.1):
+def load_dataset():
     """
     加载偏好对齐数据集
     Args:
@@ -15,6 +15,8 @@ def load_dataset(dataset_name="./data/zhihu_3k_rlhf",ratio=0.1):
         train_data: 偏好对齐训练样本
         dev_data: 偏好对齐验证样本
     """
+    dataset_name="./data/zhihu_3k_rlhf"
+    ratio=0.1
     data_zh = load_dataset(path=dataset_name)
     data_all = data_zh['train'].train_test_split(ratio)
     train_data = data_all["train"]
