49b474f2a87670e64d956ac5824ac113c2102248
赖笑慷 <laixiaokang@4paradigm.com>
2024-10-20 14:52:47 +0800

    up

--- Llama_finetune/finetune/train_lora_run.sh
+++ Llama_finetune/finetune/train_lora_run.sh
@@ -1,7 +1,7 @@
 python3 train_lora_llama.py --train_args_json ./llama2-7B_LoRA.json  \
                             --train_data_path ../data/train.jsonl  \
                             --eval_data_path ../data/dev.jsonl  \
-                            --model_name_or_path /root/autodl-fs/Llama2-Chinese-7b  \
+                            --model_name_or_path /root/autodl-fs/Llama2-Chinese-7B  \
                             --seed 42  \
                             --max_input_length 1024  \
                             --max_output_length 1024  \
